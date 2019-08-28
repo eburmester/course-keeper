28534b4edb85342a0ede1376bba1caea86f4b935
learn <burmesterelyse@gmail.com>
2019-08-28 16:51:19 -0700

    almost have submissions completely working, just need to display on page

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -1,12 +1,46 @@
+// class Submission {
+//   constructor(attributes) {
+//     this.id = attributes.id;
+//     this.content = attributes.content;
+//   }
+
+//   renderSubs() {
+//     return `
+//     <div id="submissions">
+//     <h1>Your Submissions:</h1>
+//       <li>${this.submission.content}</li>
+//     </div>
+//     `
+//   }
+// }
+
+
 document.addEventListener('turbolinks:load', () => {
   let form = document.getElementById('new_submission') 
   form && form.addEventListener('submit', (event) => {
     event.preventDefault()
-    $.ajax ({
-      url: event.target.action,
-      type: "POST",
-      data: 
-    debugger
+    let data = {submission: {}};
+     data['submission']['content'] = event.target.querySelector('#submission_content').value;
+     let token = event.target.querySelector('input[name=authenticity_token').value
+     
+     myFetch(`${event.target.action}.json`,{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'X-CSRF-token': token
+          },
+          body: JSON.stringify(data)
+        })
+          .then(res=>res.json())
+          .then(res=> console.log(res))
+          debugger
+    })
   })
-})
+
+  // const displaySubmissions = (results) => {
+  //   let html = results.content
+  //   document.getElementById('submissions').innerHTML = html
+  // }
+
 
